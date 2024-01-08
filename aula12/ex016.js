298914ae45c7519a37a5f8ddf5f0768705afeeb2
Magdiel Gomes Ferreira <magdielgomesferreira@gmail.com>
2024-01-08 16:58:30 -0300

    Add Aula12

---
+++ aula12/ex016.js
@@ -0,0 +1,15 @@
+var agora = new Date().getHours()
+
+console.log(`Agora são exatamente ${agora} horas.`)
+
+if(agora < 12)
+{
+    console.log('Bom dia!')
+}else if(agora <= 18)
+{
+    console.log('Boa Tarde!')
+}
+else
+{
+    console.log('Boa noite!')
+}
